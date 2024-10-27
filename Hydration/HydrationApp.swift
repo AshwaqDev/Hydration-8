d107fcca459ba2b1de38d5f1e96c4548b98e8ffe
AshwaqDev <Ashwag.hj@gmail.com>
2024-10-27 10:05:02 +0300

    Initial Commit

---
+++ Hydration/HydrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrationApp.swift
+//  Hydration
+//
+//  Created by Ashwaq on 24/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct HydrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HydrationTests/HydrationTests.swift
@@ -0,0 +1,17 @@
+//
+//  HydrationTests.swift
+//  HydrationTests
+//
+//  Created by Ashwaq on 24/04/1446 AH.
+//
+
+import Testing
+@testable import Hydration
+
+struct HydrationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
